6f4f00f5d5345fcdbda9ccfdde781de8bfdc577c
Jaeyeop Lee <woduq1204@gmail.com>
2020-09-18 17:50:53 +0900

    스프링 JdbcTemplate

---
+++ sql/ddl.sql
@@ -0,0 +1,6 @@
+drop table if member exists cascade;
+create table member
+(
+id bigint primary key auto_increment,
+name varcharacter(255)
+);
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,36 +1,42 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import hello.hellospring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MemberServiceTest {
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
 
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
 
-    @BeforeEach
-    void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
+    @Autowired
+    MemberRepository memberRepository;
 
-    @AfterEach
-    void afterEach() {
-        memberRepository.clearStore();
-    }
+//    @BeforeEach
+//    void beforeEach() {
+//        memberRepository = new MemoryMemberRepository();
+//        memberService = new MemberService(memberRepository);
+//    }
+
+//    @AfterEach
+//    void afterEach() {
+//        memberRepository.clearStore();
+//    }
 
     @Test
     void join() {
         // given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");
 
         // when
         Long saveId = memberService.join(member);
@@ -41,7 +47,7 @@ class MemberServiceTest {
     }
 
     @Test
-    public void DuplicateMemberException() {
+    public void duplicateMemberException() {
         // given
         Member member = new Member();
         member.setName("spring");
@@ -56,11 +62,11 @@ class MemberServiceTest {
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
-    @Test
-    void findMembers() {
-    }
+//    @Test
+//    void findMembers() {
+//    }
 
-    @Test
-    void findOne() {
-    }
+//    @Test
+//    void findOne() {
+//    }
 }
